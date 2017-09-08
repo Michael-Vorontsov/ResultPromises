7eaed9fec3ee2866c1b3763456d8fae2d7c96a2a
Michael Vorontsov <michel06@ukr.net>
2017-09-08 19:22:02 +0100

    Example code cleanup

--- Example/iOS/iOS/ViewController.swift
+++ Example/iOS/iOS/ViewController.swift
@@ -25,7 +25,9 @@ final class ViewController: UITableViewController {
   }
   
   @IBAction func pullToRefreshAction(_ sender: Any) {
-    reloadData()
+    reloadData(delay: 2.0, errorSimulationCounter: 5)
+//    oldReload()
+//    reloadData()
   }
   
   func resetUsers() {
@@ -37,60 +39,48 @@ final class ViewController: UITableViewController {
     tableView.endUpdates()
   }
   
-  
-  
-  fileprivate func showError(message: String) {
-    let alert = UIAlertController(title: "Network connection required!", message: message, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-    self.present(alert, animated: true, completion: nil)
-  }
-  
+  //* Reload data old manner
   func oldReload() {
     let request = URLRequest(url: URL(string: "https://jsonplaceholder.typicode.com/users")!)
     session.dataTask(with: request) { (data, response, error) in
-      self.tableView.refreshControl?.endRefreshing()
-      if let error = error {
-        self.showError(message: error.localizedDescription)
-        return
-      }
-      
-      let httpResponse = response as! HTTPURLResponse
-      
-      guard (200...299).contains( httpResponse.statusCode) else {
-        self.showError(message: "Code: \(httpResponse.statusCode)")
-        return
-      }
-      
-      guard let data = data else {
-        self.showError(message: "Data is Empty!")
-        return
-      }
-      
-      let decoder = JSONDecoder()
-      do {
-        let users =  try decoder.decode([User].self, from: data)
-        self.users = users
-        DispatchQueue.main.async {
-          self.tableView.reloadData()
+      DispatchQueue.main.async {
+        self.tableView.refreshControl?.endRefreshing()
+        if let error = error {
+          self.showError(message: error.localizedDescription)
+          return
         }
-      } catch {
-        self.showError(message: error.localizedDescription)
+        
+        let httpResponse = response as! HTTPURLResponse
+        
+        guard (200...299).contains( httpResponse.statusCode) else {
+          self.showError(message: "Code: \(httpResponse.statusCode)")
+          return
+        }
+        
+        guard let data = data else {
+          self.showError(message: "Data is Empty!")
+          return
+        }
+        
+        let decoder = JSONDecoder()
+        do {
+          let users =  try decoder.decode([User].self, from: data)
+          self.users = users
+          self.tableView.reloadData()
+        } catch {
+          self.showError(message: error.localizedDescription)
+        }
       }
-      
-    }.resume()
+      }.resume()
   }
   
+  // Reload data simples way
   func reloadData() {
     self.tableView.refreshControl?.beginRefreshing()
     resetUsers()
-//    self.oldReload()
     URLRequest.requestFor(path: "https://jsonplaceholder.typicode.com/users")
-      .then {(request) -> Promise<Data> in
-        return self.session.fetchData(from: request)
-      }
-      .then { (data) -> [User] in
-        let decoder = JSONDecoder()
-        return try decoder.decode([User].self, from: data)
+      .then {(request) -> Promise<[User]> in
+        return self.session.fetchRESTObject(from: request)
       }
       .onComplete { (_) in
         self.tableView.refreshControl?.endRefreshing()
@@ -101,13 +91,13 @@ final class ViewController: UITableViewController {
       }
       .onError { (error) in
         self.showError(message: error.localizedDescription)
-      }
+    }
   }
-
-  func reloadData1() {
+  
+  //* Reload data, with delay, and failing with error
+  func reloadData(delay: Double, errorSimulationCounter: Int) {
     self.tableView.refreshControl?.beginRefreshing()
     resetUsers()
-    //    self.oldReload()
     URLRequest.requestFor(path: "https://jsonplaceholder.typicode.com/users")
       .then {(request) -> Promise<[User]> in
         return self.session.fetchRESTObject(from: request)
@@ -115,7 +105,7 @@ final class ViewController: UITableViewController {
       // Add delay for better illustration
       .then{ (users) -> Promise<[User]> in
         let promise = Promise<[User]>()
-        DispatchQueue.global(qos: .default).asyncAfter(deadline: .now() + 2.0) {
+        DispatchQueue.global(qos: .default).asyncAfter(deadline: .now() + delay) {
           promise.resolve(result: users)
         }
         return promise
@@ -123,11 +113,11 @@ final class ViewController: UITableViewController {
       // Add probability to fail
       .then{ (users) -> [User] in
         self.reloadCount += 1
-        if 0 == self.reloadCount % 5 {
+        if 0 == self.reloadCount % errorSimulationCounter {
           self.title = "Error simulation"
           throw ViewControllerError.simulatedError
         }
-        self.title = "Pull \(5 - self.reloadCount % 5) more time to get error"
+        self.title = "Pull \(5 - self.reloadCount % errorSimulationCounter) more time to get error"
         return users
       }
       .onComplete { (_) in
@@ -138,12 +128,18 @@ final class ViewController: UITableViewController {
         self.tableView.reloadData()
       }
       .onError { (error) in
-        let alert = UIAlertController(title: "Network connection required!", message: error.localizedDescription, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        self.showError(message: error.localizedDescription)
     }
   }
-
+  
+  fileprivate func showError(message: String) {
+    let alert = UIAlertController(title: "Network connection required!", message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+    self.present(alert, animated: true, completion: nil)
+  }
+  
+  //MARK: TableView
+  
   override func numberOfSections(in tableView: UITableView) -> Int {
     return users.count > 0 ? 1 : 0
   }
@@ -163,7 +159,7 @@ final class ViewController: UITableViewController {
     cell.tag = name.hashValue
     URLRequest.requestFor(path: "https://robohash.org/\(name)")
       .then {(request) -> Promise<Data> in
-          self.session.fetchData(from: request)
+        self.session.fetchData(from: request)
       }
       .then { (data) -> UIImage in
         guard let image = UIImage(data: data) else {
@@ -186,9 +182,10 @@ final class ViewController: UITableViewController {
           cell.imageView?.image = image
           cell.setNeedsLayout()
         }
-      }
+    }
     
     return cell
   }
-
+  
+  
 }
