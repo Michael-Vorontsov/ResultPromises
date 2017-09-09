14694a738ff4c839639eb5511156d90fb2c1ec14
Michael Vorontsov <michel06@ukr.net>
2017-09-09 16:00:04 +0100

    Add Changelog and swift package manager file. Move metafiles into separate virtual group.

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  ResultPromises
+//
+//  Created by Mykhailo Vorontsov on 9/9/17.
+//  Copyright © 2017 Mykhailo Vorontsov. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "ResultPromises",
+  exclude: ["Example"]
+)
